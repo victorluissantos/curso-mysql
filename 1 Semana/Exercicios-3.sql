7890cf7fb5b54dc5aa4bc2cc007823584d765aa1
Victor L. Santos <victorluissantos@live.com>
2020-06-19 21:55:48 -0300

    upper

---
+++ 1 Semana/Exercicios-3.sql
@@ -0,0 +1,95 @@
+/* 1. Quem mais empresta livro, homem ou mulher ? */
+SELECT 
+    COUNT(u.genero) as 'total',
+    u.genero
+FROM
+	emprestimos e
+        JOIN
+    usuarios u ON e.usuario_id = u.id
+WHERE
+	u.genero <> 'Indefinido'
+    AND
+    e.data_entrega IS NULL
+GROUP BY u.genero
+ORDER BY 1 DESC
+LIMIT 1;
+
+/* 2. Qual o Estado que mais empresta livros ? */
+SELECT 
+    count(u.uf) as 'total',
+    u.uf
+FROM
+    emprestimos e
+    INNER JOIN
+    usuarios u ON e.usuario_id = u.id
+WHERE
+	u.uf is not null
+GROUP BY 2 DESC
+ORDER BY 1 DESC
+LIMIT 1;
+
+/* Pegando todos atrasados ate os que ja entregam */
+SELECT 
+    e.data_retirada AS 'data retirada'
+FROM
+    emprestimos e
+WHERE
+    (e.data_entrega IS NULL
+        AND DATE_ADD(e.data_retirada,
+        INTERVAL 3 DAY) < NOW())
+        OR DATEDIFF(e.data_entrega, e.data_retirada) > 3
+
+
+/* 3. Qual o valor da maior multa no sistema */
+SELECT 
+    e.data_retirada AS 'data retirada',
+    e.data_retirada,
+    DATEDIFF(e.data_entrega,
+            DATE_ADD(e.data_retirada,
+                INTERVAL 3 DAY)) AS 'dias de atraso',
+    (SELECT 
+            t.multa
+        FROM
+            tarifas t) AS 'multa',
+    (SELECT 
+            ((DATEDIFF(e.data_entrega,
+                        DATE_ADD(e.data_retirada,
+                            INTERVAL 3 DAY))) * t.diaria)
+        FROM
+            tarifas t) AS 'mora',
+    (SELECT 
+            SUM((DATEDIFF(e.data_entrega,
+                        DATE_ADD(e.data_retirada,
+                            INTERVAL 3 DAY))) * t.diaria)+t.multa
+        FROM
+            tarifas t) AS 'Total a pagar'
+FROM
+    emprestimos e
+WHERE
+    (e.data_entrega IS NULL
+        AND DATE_ADD(e.data_retirada,
+        INTERVAL 3 DAY) < NOW())
+        OR (DATEDIFF(e.data_entrega, e.data_retirada) > 3
+        AND DATEDIFF(e.data_entrega, e.data_retirada) IS NOT NULL)
+ORDER BY 6 DESC
+LIMIT 1;
+
+/* 4. Qual o usuario com o maior valor de multa em aberto */
+
+SELECT 
+    u.nome,
+    SUM((SELECT 
+            ((t.diaria * (DATEDIFF(NOW(),
+                        DATE_ADD(e.data_retirada,
+                            INTERVAL 3 DAY)))) + t.multa)
+        FROM
+            tarifas t)) AS 'multa'
+FROM
+    usuarios u
+        INNER JOIN
+    emprestimos e ON u.id = e.usuario_id
+WHERE
+    e.data_entrega IS NULL
+GROUP BY u.nome
+ORDER BY 2 DESC
+limit 1;
--- 1 Semana/querys.sql
+++ 1 Semana/querys.sql
@@ -25,3 +25,59 @@ ALTER TABLE `ebiblioteca`.`livros` CHANGE COLUMN `situacao` ENUM('Disponivel', '
 /* Criando index */
 ALTER TABLE `ebiblioteca`.`livros` 
 ADD INDEX `idx_titulo` (`titulo` ASC);
+
+/* Criando index da situcao */
+ALTER TABLE `ebiblioteca`.`livros` 
+ADD INDEX `idx_situacao` (`situacao` ASC);
+
+/* Criando tabela de tarifas */
+CREATE TABLE `ebiblioteca`.`tarifas` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `multa` DECIMAL(3,2) NULL,
+  `diaria` DECIMAL(3,2) NULL,
+  `data_cadastro` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`));
+
+
+/* Calculando atraso por dia atraso */
+SELECT 
+    e.*,
+    (
+		SELECT multa FROM tarifas
+    ) as 'mora',
+    (
+		SELECT (DATEDIFf(now(), DATE_ADD(e.data_retirada, INTERVAL 3 DAY))*diaria) FROM tarifas
+    ) as 'multa',
+    DATEDIFf(now(), DATE_ADD(e.data_retirada, INTERVAL 3 DAY)) as 'dias de atraso'
+FROM
+    emprestimos e
+WHERE
+	e.id = 12
+
+
+/* Calcula multa, mora e total a pagar */
+SELECT 
+    e.*,
+    (SELECT 
+            multa
+        FROM
+            tarifas) AS 'mora',
+    (SELECT 
+            (DATEDIFF(NOW(),
+                        DATE_ADD(e.data_retirada,
+                            INTERVAL 3 DAY)) * diaria)
+        FROM
+            tarifas) AS 'multa',
+    (SELECT 
+            SUM((DATEDIFF(NOW(),
+                        DATE_ADD(e.data_retirada,
+                            INTERVAL 3 DAY)) * diaria) + multa)
+        FROM
+            tarifas) AS 'Total a pagar multa',
+    DATEDIFF(NOW(),
+            DATE_ADD(e.data_retirada,
+                INTERVAL 3 DAY)) AS 'dias de atraso'
+FROM
+    emprestimos e
+WHERE
+    e.id = 12
